b5db44d17396a1b6a770c63ce721005809f7346a
AbdulRehman6996 <rehman.2511@yahoo.in>
2018-07-11 02:44:21 +0530

    All the testing work of Fedora 28 installtion done

---
+++ tess.v
@@ -0,0 +1,27 @@
+tabkey and the enterkey to click on the installTOHardrive 
+Type english ,(the cursor already blinks int the text box)
+now hit tab key 3 times and hit enter 
+and again 4 tab and enter to click done
+
+installation summary
+one tab and enter to move in the keyboard selection window or hit enter if u want default as english
+
+the again tab and enter to select the time and date or hit enter if default
+
+again tab and enter to selet installation destination and hit enter if all is ok
+
+6 tab and enter key to begin the installation
+
+now 2 tab and hit enter
+
+windows key and type terminal and hit enter and then type reboot
+
+now after tht press enterkey on first bootloader screen
+
+
+now on the setting screen two tabs to and hit enter...again hit enter...and tab and enter
+now type redhat and hit enter ..now type the password and hit enter and again hit enter....
+	
+
+	now it will take u to the login screen with bydefault redhat selected so hit enter type the password and hit enter...and start using fedora
+
--- tests/installationProcess.pm
+++ tests/installationProcess.pm
@@ -5,11 +5,113 @@ use testapi;
 sub run
 {
 	
-	assert_screen "bootloader",15;
+	assert_screen "bootloader",30;
 
 	send_key "ret";
 
-	assert_screen "Welcome to Fedora Screen",300;
+	assert_screen "WelcomeToFedoraScreen",10000;
+	send_key "tab";
+	send_key "ret";
+
+
+	assert_screen "InstallationLanguageSelection",10000;
+	type_string "English",100;
+
+	#To select the English as installation language
+	send_key "up";
+	send_key "tab";
+	send_key "tab";
+	send_key "tab";
+	send_key "tab";
+	send_key "ret";
+
+	assert_screen "InstallationSummary",10000;
+
+	#To select the installation partition & keeping keyboard & region default
+	wait_idle 10;
+	send_key "tab";
+        send_key "tab";
+        send_key "tab";
+        send_key "ret";
+	wait_idle 10;
+        send_key "ret";
+	wait_idle 20;
+        send_key "down";
+	send_key "down";
+        send_key "down";
+
+        send_key "right";
+
+        send_key "ret";
+       
+	#Software & other Stuff installation window
+	assert_screen "ConfigurationInstallationScreen",500;
+	
+	#Wait for approx. 13 minutes to get installation complete
+	wait_idle 800;
+	
+	#After installation completed pressing Quit button
+	send_key "down";
+	send_key "down";
+	send_key "ret";
+	
+	#Back to desktop after installation
+	assert_screen "FedoraInstallation",100;
+
+	#Rebooting the system to finalize installation and creating user
+	send_key "super";
+	wait_idle 10;
+	type_string "Terminal",100;
+	wait_idle 10;
+	send_key "ret";
+	wait_idle 10;
+	type_string "reboot";
+	wait_idle 10;
+	send_key "ret";
+
+	#Going through Setup screen 
+	assert_screen "SetupScreen",300;
+	send_key "tab";
+	send_key "tab";
+	send_key "ret";
+
+	#Priacy screen
+	assert_screen "Privacy",15;
+	wait_idle 10;
+	send_key "ret";
+
+	#Online accounts screen, we are just skipping it
+	assert_screen "OnlineAccounts",15;
+	wait_idle 10;
+	send_key "right";
+	send_key "ret";
+
+	#Setting up username i.e About you screen
+	assert_screen "UserSetup",15;
+	wait_idle 10;
+	type_string "Univarsal";
+	hold_key "shift";
+	send_key "tab";
+	send_key "tab";
+	release_key "shift";
+	send_key "ret";
+
+	#Password setup screen
+	type_password "UniversalFedora";
+	send_key "tab";
+	type_password "UniversalFedora";
+	hold_key "shift";
+	send_key "tab";
+	send_key "tab";
+	release_key "shift";
+	send_key "ret";
+
+	#Ready to use Fedora 28
+	assert_screen "ReadyToGo",60;
+	send_key "ret";
+
+
+
 }
 sub test_flags
 {
